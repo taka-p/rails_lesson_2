40f9b19c395fc70adc5229bd87b7e5302732074f
TakashiSato <takashisato.235711@gmail.com>
2015-05-06 22:01:58 +0900

    refs deviceによるsign_upページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'users/show'
-
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :users, only: [:show]
-  devise_for :users
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
