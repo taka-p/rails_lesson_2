f0216e602a2dbe74f2b06e1c828eca252ffac7a4
TakashiSato <takashisato.235711@gmail.com>
2015-06-07 15:45:04 +0900

    refs 続sesstion周りのエラー解消　route.rb修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
