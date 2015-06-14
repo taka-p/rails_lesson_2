fdfac1d2e1bddd23fab8d9fb14268445cee9392a
TakashiSato <takashisato.235711@gmail.com>
2015-06-14 11:28:55 +0900

    refs seession周りのエラー完全解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
-  before_create :create_remember_token
+  #before_create :create_remember_token
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -15,9 +15,9 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
-  private
+  # private
 
-    def create_remember_token
-      self.remember_token = User.encrypt(User.new_remember_token)
-    end
+  #   def create_remember_token
+  #     self.remember_token = User.encrypt(User.new_remember_token)
+  #   end
 end
