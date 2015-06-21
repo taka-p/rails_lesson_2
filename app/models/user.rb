414c5a267e56f8db9451d28017967fa17369588b
TakashiSato <takashisato.235711@gmail.com>
2015-06-21 13:48:52 +0900

    refs Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   validates :name,  presence: true, length: { maximum: 50 }
 
+  def feed
+    # このコードは準備段階です。
+    # 完全な実装は、次回以降に行います。
+    Micropost.where("user_id = ?", id)
+  end
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
